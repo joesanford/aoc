55d87c8d1038d74aed3861b429c497f2897cba2d
Joe Sanford <josephsanford@gmail.com>
2021-12-02 10:01:09 -0500

    Added both parts of day2 solution

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -3,20 +3,42 @@
 with open('input.txt') as f:
     input_data = f.read()
 
-horizontal = 0
-depth = 0
-aim = 0
+def part1(input_data):
+    horizontal = 0
+    depth = 0
 
-for line in input_data.splitlines():
-    command, amount = line.split(' ')
-    amount = int(amount)
+    for line in input_data.splitlines():
+        command, amount = line.split(' ')
+        amount = int(amount)
 
-    if command == 'forward':
-        horizontal += amount
-        depth += (amount * aim)
-    elif command == 'up':
-        aim -= amount
-    elif command == 'down':
-        aim += amount
+        if command == 'forward':
+            depth += amount
+        elif command == 'up':
+            horizontal -= amount
+        elif command == 'down':
+            horizontal += amount
+    
+    print(horizontal * depth)
 
-print(horizontal * depth)
+def part2(input_data):
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for line in input_data.splitlines():
+        command, amount = line.split(' ')
+        amount = int(amount)
+
+        if command == 'forward':
+            horizontal += amount
+            depth += (amount * aim)
+        elif command == 'up':
+            aim -= amount
+        elif command == 'down':
+            aim += amount
+
+    print(horizontal * depth)
+
+
+part1(input_data)
+part2(input_data)
